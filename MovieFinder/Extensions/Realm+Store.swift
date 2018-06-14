8d01cd288223bef752fb615fd9d1019024351333
D'Alberti, Luca <luca.dalberti@merck.com>
2018-06-14 17:19:55 +0200

    Added default implementation of Store and Storeable to Realm

---
+++ MovieFinder/Extensions/Realm+Store.swift
@@ -0,0 +1,35 @@
+//
+//  Realm+Store.swift
+//  MovieFinder
+//
+//  Created by D'Alberti, Luca on 6/12/18.
+//  Copyright © 2018 dalu93. All rights reserved.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+// MARK: - Realm Object extension for Storeable
+extension RealmSwift.Object: Storeable {
+    func save(using store: Realm) throws {
+        try store.save(self)
+    }
+
+    func delete(using store: Realm) throws {
+        try store.deletePermanently(self)
+    }
+}
+
+// MARK: - Realm extension for Store
+extension Realm: Store {
+    func save(_ object: RealmSwift.Object) throws {
+        try self.write {
+            self.add(object)
+        }
+    }
+
+    func deletePermanently(_ object: RealmSwift.Object) throws {
+        self.delete(object)
+    }
+}
